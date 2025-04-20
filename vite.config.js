541ec279b29f366aa3bf8951ad32eb3179fc8d2b
ComputeAce <timeastw@gmail.com>
2025-04-20 11:33:20 +0200

    m:

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/', // 👈 Important for GitHub Pages
+  base: '/vite-blog', 
 
   server: {
     port: 4000,
