e96dc689376a35f4795513b77087fdbace9d0e65
ComputeAce <timeastw@gmail.com>
2025-04-20 10:27:39 +0200

    Set correct base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/vite-blog/', // 👈 Important for GitHub Pages
 
   server: {
     port: 4000,
   },
   plugins: [react(), tailwindcss()],
-
 })
